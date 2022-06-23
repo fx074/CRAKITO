065074871a0317bbb9154933cd939c88a073bc45
Oscar <oscar.vilmot@gmail.com>
2022-06-23 20:17:13 +0200

    edit app router

--- crackito/src/App.vue
+++ crackito/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">à propos</router-link>
+    <router-link to="/upload">Envoyer un fichier</router-link>
   </nav>
   <router-view/>
 </template>
