c3bdfbfaae5d520a2fa1b036375a02af3e24d62a
OscarLeSuperBGGigaFITQuiVaALaSalle <oscar.vilmot@gmail.com>
2022-06-24 11:29:41 +0200

    Submit form and start ciphering

--- crackito/src/views/upload-main.vue
+++ crackito/src/views/upload-main.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <div>
       <label>File
-        <input type="file" name="file" id="file" v-on:change="FileUploadOnchange()" />
+        <input type="file" ref="file" v-on:change="FileUploadOnchange( $event )" />
       </label>
       <button v-on:click="submitForm()">Upload</button>
     </div>
@@ -17,8 +17,20 @@
 export default {
   methods: {
     FileUploadOnchange() {
-      return null;
+      [this.file] = this.$refs.file.files;
     },
+    submitForm() {
+      console.log(this.file);
+    }, /*
+    cipherFile() {
+      let reader = new FileReader();
+    }, */
+  },
+  data() {
+    return {
+      file: '',
+      result: '',
+    };
   },
 };
 </script>
