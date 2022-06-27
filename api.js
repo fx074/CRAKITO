f21444e0e8a9ca0d4311981f27d6a3e09c1737fa
Maxime Laze <maxime.laze@efrei.net>
2022-06-27 10:46:57 +0200

    Prototype Multer

---
+++ api.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const multer = require('multer')
+const req = require("express/lib/request")
+const res = require("express/lib/response")
+const path = require('path')
+
+const app = express()
+
+const port = 3000;
+
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname, 'test/test.html'))
+});
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname,'/Files'))
+    },
+})
+const upload = multer({ storage: storage })
+
+app.post('/Upload', upload.any(), function(req, res, next){
+    res.send(req.file)
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
