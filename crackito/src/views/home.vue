1d078b0844100a8bf4cc43773b12cb165ecc71c8
Elia <elia.zhang@efrei.net>
2022-06-29 12:25:02 +0200

    Edition du fichier Receive et modification du header

--- crackito/src/views/home.vue
+++ crackito/src/views/home.vue
@@ -8,7 +8,7 @@
                 </div>
                 <div class="m_right">
                     <nav>
-                      <router-link to="/" class="nav_prim">Accueil </router-link>
+                      <router-link to="/" class="nav_link">Accueil </router-link>
                       <router-link to="/upload" class="nav_prim">Envoyer un fichier </router-link>
                       <router-link to="/receive" class="nav_prim">Recevoir un fichier </router-link>
                     </nav>
---
+++ crackito/src/views/receive-main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+        <header>
+        <div class="menu">
+            <div class="inner">
+                <div class="m_left">
+                    <img src="../assets/logo_titre.png" alt="Le logo du site" class="m_logo" >
+                </div>
+                <div class="m_right">
+                    <nav>
+                      <router-link to="/" class="nav_prim">Accueil </router-link>
+                      <router-link to="/upload" class="nav_prim">Envoyer un fichier </router-link>
+                      <router-link to="/receive" class="nav_link">Recevoir un fichier</router-link>
+                    </nav>
+                    <router-view />
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container_sf">
+      <div class="send_file">
+        <p class="sf_title">Récupérer un fichier</p>
+        <p class="sf_des">Un façon simple et sécurisé de partager et récupérer vos fichiers</p>
+        <div class="sf_contour">
+          <form action="/Upload" method="post" enctype="multipart/form-data" class="sf_form">
+            <div class="sf_button"></div>
+            <label>Boum shakalaka<input type="file" name="TP7"></label>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    @import '../assets/style/ReceiveStyle.css';
+</style>
