fe33cd9a23fe05ee6f0185da589c6c0aa7748f56
PaSarco <oscar.vilmot@gmail.com>
2022-06-30 23:06:44 +0200

    FIX API

--- api.js
+++ api.js
@@ -1,33 +1,33 @@
 const express = require('express')
-const path = require('path')
-var cors = require('cors')
 const multer = require('multer')
-
+const path = require('path')
+var cors = require('cors');
 
 const app = express()
 app.use(cors())
 
 const port = 3000;
 
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname, 'test/test.html'))
+});
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, path.join(__dirname,'/files'))
     },
 })
-const upload = multer({ storage: storage })
-
-app.post('/upload',upload.any(), uploadFiles);
-
-function uploadFiles(req, res) {
-    console.log(req.files)
-    console.log(req.body)
-    res.json({ message: "Successfully uploaded files" })
-}
+const upload = multer({ dest: "files/" })
 
 app.post('/test', (req, res) => {
     res.send('test');
 })
 
+app.post('/upload',upload.any(), uploadFiles);
+
+function uploadFiles(req, res) {
+    console.log(req);
+}
 
 app.listen(port, () => {
     console.log(`http listening on port ${port}`)
