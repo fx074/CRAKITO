a59ef979456700a80041e9df5856d9c8c5c449b8
PaSarco <oscar.vilmot@gmail.com>
2022-06-30 22:53:37 +0200

    change API post reception

--- api.js
+++ api.js
@@ -1,33 +1,31 @@
 const express = require('express')
-const multer = require('multer')
 const path = require('path')
-var cors = require('cors');
+var cors = require('cors')
+const multer = require('multer')
+const upload = multer({ storage: storage })
+
 
 const app = express()
 app.use(cors())
-
 const port = 3000;
-
-app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname, 'test/test.html'))
-});
-
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, path.join(__dirname,'/files'))
     },
 })
-const upload = multer({ storage: storage })
+
+app.post('/upload',upload.any(), uploadFiles);
+
+function uploadFiles(req, res) {
+    console.log(req.files)
+    console.log(req.body)
+    res.json({ message: "Successfully uploaded files" })
+}
 
 app.post('/test', (req, res) => {
     res.send('test');
 })
 
-app.post('/upload', uploadFiles);
-
-function uploadFiles(req, res) {
-    console.log(req.body);
-}
 
 app.listen(port, () => {
     console.log(`http listening on port ${port}`)
