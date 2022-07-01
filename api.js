25a32e01416ac5e2f4aea58a0d03a8bfdac66296
PaSarco <oscar.vilmot@gmail.com>
2022-07-01 16:06:00 +0200

    change filename and storage directory API

--- api.js
+++ api.js
@@ -14,8 +14,11 @@ const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, path.join(__dirname,'/files'))
     },
+    filename: function(req, file, cb) {
+        cb(file.fieldname);
+    },
 })
-const upload = multer({ dest: "files/" })
+const upload = multer({ storage: storage })
 
 app.post('/test', (req, res) => {
     res.send('test');
