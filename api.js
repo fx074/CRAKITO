e3025013ae5f96293febf12e07bec05f9b090646
PaSarco <oscar.vilmot@gmail.com>
2022-07-01 15:03:14 +0200

    .any() post

---
+++ api.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = 3000;
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname,'/files'))
+    },
+})
+const upload = multer({ dest: "files/" })
+
+app.post('/test', (req, res) => {
+    res.send('test');
+})
+
+app.post('/upload', upload.any(), uploadFiles);
+
+function uploadFiles(req, res) {
+    console.log(req.toString() + '\n\n')
+    res.json({ message: "Successfully uploaded files" })
+}
+
+app.listen(port, () => {
+    console.log(`http listening on port ${port}`)
+})
