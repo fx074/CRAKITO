82ecc3712f6d8aca901e96a1d0530e04a61d8624
PaSarco <oscar.vilmot@gmail.com>
2022-07-04 16:28:24 +0200

    fix API where request

--- api.js
+++ api.js
@@ -91,7 +91,9 @@ async function dlFiles(req, res) {
         const path_dl = file[0].dataValues.path
         const fileName_dl = file[0].dataValues.path.split('ito/').pop().split('.encr')[0]+'.encr'
         let file = await File.findAll({
-            where: ref,
+            where: {
+                ref: ref,
+            },
         })
         fs.readFile(path_dl, 'utf8', (err, data) => {
             if(err) {
